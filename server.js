128aa3e8e2152b9e7494c55943e0e850a2f13a93
Luis Rosario <luisrosariomix@gmail.com>
2019-08-18 09:04:44 -0700

    Added Socket IO Sever Setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require(`dotenv`).config()
 //Express Setup
 const express = require(`express`)
+const app = express()
 //Socket IO
 const socketIO = require('socket.io')
 //MediaSoup Configuration
@@ -10,29 +11,27 @@ const config = require('./config')
 //File system
 const fs = require('fs')
 //Express Secured Server
-const https = require('https')
+const server = require('https')
 
 //Configuration
 const PORT = process.env.PORT || 3002
 
 //Global Variables
 let worker
-let webServer
+let https
 let socketServer
-let expressApp
 let producer
 let consumer
 let producerTransport
 let consumerTransport
 let mediasoupRouter
 
-//Run Express
-const runExpressApp = async () => {
-  expressApp = express()
-  expressApp.use(express.json())
-  expressApp.use(express.static(__dirname))
+//Handle Express Configuration
+const runExpressApp = async (app) => {
+  app.use(express.json())
+  app.use(express.static(__dirname))
 
-  expressApp.use((error, req, res, next) => {
+  app.use((error, req, res, next) => {
     if (error) {
       console.warn('Express app error,', error.message)
       error.status = error.status || (error.name === 'TypeError' ? 400 : 500)
@@ -43,8 +42,8 @@ const runExpressApp = async () => {
     }
   })
 }
-//Start Server
-const runWebServer =  async () => {
+//Handle HTTPS Server
+const runHttpsServer =  async () => {
   const { sslKey, sslCrt } = config
 
   if (!fs.existsSync(sslKey) || !fs.existsSync(sslCrt)) {
@@ -57,35 +56,65 @@ const runWebServer =  async () => {
     key: fs.readFileSync(sslKey),
   }
 
-  webServer = https.createServer(tls, expressApp)
+  https = server.createServer(tls, app)
 
-  webServer.on('error', (err) => {
+  https.on('error', (err) => {
     console.error('starting web server failed:', err.message)
   })
 
   await new Promise((resolve) => {
     const { listenIp, listenPort } = config
 
-    webServer.listen(PORT , listenIp, () => {
+    https.listen(PORT , listenIp, () => {
 
       const listenIps = config.mediasoup.webRtcTransport.listenIps[0]
       const ip = listenIps.announcedIp || listenIps.ip
 
       console.log(`I'm listening to port ${ PORT }`)
       console.log('server is running')
-      console.log(`open https//${ ip }:${ PORT } in your web browser`)
+      console.log(`open https://${ ip }:${ PORT } in your web browser`)
 
       resolve()
     })
   })
 }
 
-( async () => {
+//Bind SocketIO to https server
+const runSocketServer = async () => {
+  socketServer = socketIO(https, {
+    serveClient: false,
+    path: '/server',
+    log: false,
+  })
+}
+
+//Create Media Worker To Host Routers
+const runMediasoupWorker = async () => {
+  worker = await mediasoup.createWorker({
+    logLevel: config.mediasoup.worker.logLevel,
+    logTags: config.mediasoup.worker.logTags,
+    rtcMinPort: config.mediasoup.worker.rtcMinPort,
+    rtcMaxPort: config.mediasoup.worker.rtcMaxPort,
+  })
+
+  worker.on('died', () => {
+    console.error('mediasoup worker died, exiting in 2 seconds... [pid:%d]', worker.pid)
+    setTimeout(() => process.exit(1), 2000);
+  })
+
+  const mediaCodecs = config.mediasoup.router.mediaCodecs;
+  mediasoupRouter = await worker.createRouter({ mediaCodecs })
+}
+
+//Time to launch server
+( async (app) => {
   try {
-    await runExpressApp()
-    await runWebServer()
+    await runExpressApp(app)
+    await runHttpsServer()
+    await runSocketServer()
+    await runMediasoupWorker()
 
   } catch (err) {
     console.error(err)
   }
-})()
+})(app)
